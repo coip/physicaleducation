fb3590095bd57d4930cd0f97432b853145a36d36
ethan c <ethan.coyle@cuc.org>
2018-01-08 17:47:59 -0600

    whoop

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# gope
+# pe in go
+some may use random linux built-ins not packaged.
---
+++ pe2.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+var alpha, beta, omega int = 1, 2, 3
+
+func next(a, b int) (o int) {
+	o = a + b
+	return
+}
+
+func iterate() {
+	alpha = beta
+	beta = omega
+	omega = next(alpha, beta)
+}
+
+func main() {
+
+	sum := 0
+	for omega < 4000000 {
+		if alpha % 2 == 0 {
+			sum += alpha
+		}
+		iterate()
+	}
+	if beta % 2 == 0 { sum += beta }
+	if omega % 2 == 0 { sum += omega }
+
+	fmt.Printf("sum: %v\n", sum)
+}
