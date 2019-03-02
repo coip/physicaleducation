06546901201364d4fd2794caefb2f9aca0676cd6
coip <e@coip.me>
2019-03-02 15:37:54 -0600

    added 205, neg is peter wins pos is collin wins

---
+++ pe205.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	cdicecount = 6
+	pdicecount = 9
+	cdicesize  = 6
+	pdicesize  = 4
+)
+
+func makeTimestamp() int64 {
+	return time.Now().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
+}
+
+func init() {
+	rand.Seed(makeTimestamp())
+}
+func rollplayer(c, r int) int {
+	var cpoints = 0
+//	fmt.Println("##   rolling", c, r)
+	for croll := 1; croll <= c; croll++ {
+		randomint := 1 + rand.Intn(r)
+//		fmt.Printf("rolled a %d\n", randomint)
+		cpoints += randomint
+
+	}
+//	fmt.Println("done rolling", c, r)
+	return cpoints
+}
+func main() {
+	fmt.Println(rollplayer(cdicecount, cdicesize) - rollplayer(pdicecount, pdicesize))
+}
